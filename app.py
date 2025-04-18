9ba541fa49767515f914c8c7687176c72876d118
abhishek-codes55 <abhishek@crazibrainsolutions.com>
2025-04-18 21:56:09 +0530

    Requirements

--- app.py
+++ app.py
@@ -5,12 +5,11 @@ from dotenv import load_dotenv
 
 load_dotenv() 
 
-print(os.getenv("GEMINI_API_KEY"))
 
 app = Flask(__name__)
 
 client = OpenAI(
-    api_key=os.getenv("GEMINI_API_KEY"),
+    api_key=os.getenv("OPENAI_API_KEY"),
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
 )
 
